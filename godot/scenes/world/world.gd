ca3e1fc01f81d9432865087faa26421a38a9b957
John <jastadj@gmail.com>
2022-07-25 03:25:37 -0700

    floor type meshes

--- godot/scenes/world/world.gd
+++ godot/scenes/world/world.gd
@@ -2,16 +2,45 @@ extends Spatial
 
 onready var _cells = $cells
 
+var _cells_y = 0
+var _cells_x = 0
+
 func _ready():
 	
 	build_world_level(UW.data["uw1"], 0)
 
 func build_world_level(uwdata, levelnum):
 	
-	var floormesh = preload("res://scenes/world/cell_shapes/floor.tscn")
+	var floormeshes
 	var tilesize = 2
 	var map = uwdata["map"]
 	var level = map["levels"][levelnum]
+	_cells_y = level["cells"].size()
+	_cells_x = level["cells"][0].size()
+	
+	# cell types
+	#00      Solid (wall tile)
+	#01      Open (square tile of empty space)
+	#02      Diagonal, open SE
+	#03      Diagonal, open SW
+	#04      Diagonal, open NE
+	#05      Diagonal, open NW
+	#06      Sloping up to the north
+	#07      Sloping up to the south
+	#08      Sloping up to the east
+	#09      Sloping up to the west
+	floormeshes = [
+				null,
+				preload("res://scenes/world/cell_shapes/floor.tscn").instance(),
+				preload("res://scenes/world/cell_shapes/floor_diag_se.tscn").instance(),
+				preload("res://scenes/world/cell_shapes/floor_diag_sw.tscn").instance(),
+				preload("res://scenes/world/cell_shapes/floor_diag_ne.tscn").instance(),
+				preload("res://scenes/world/cell_shapes/floor_diag_nw.tscn").instance(),
+				preload("res://scenes/world/cell_shapes/floor_slope_n.tscn").instance(),
+				preload("res://scenes/world/cell_shapes/floor_slope_s.tscn").instance(),
+				preload("res://scenes/world/cell_shapes/floor_slope_e.tscn").instance(),
+				preload("res://scenes/world/cell_shapes/floor_slope_w.tscn").instance()
+				]
 	
 	# clear everything in the cells node
 	for c in _cells.get_children():
@@ -19,8 +48,9 @@ func build_world_level(uwdata, levelnum):
 		c.queue_free()
 	
 	# build floor map from level data cells
-	for y in range(0, level["cells"].size()):
-		for x in range(0, level["cells"][y].size()):
+	for y in range(0, _cells_y):
+		for x in range(0, _cells_x):
+			
 			var cell = level["cells"][y][x]
 			var type = cell["type"]
 			var height = cell["floor_height"]
@@ -28,7 +58,9 @@ func build_world_level(uwdata, levelnum):
 			# if solid cell, do nothing
 			if type == 0: continue
 			
-			var newmesh = floormesh.instance()
+			# copy mesh from floor mesh types and set
+			# the material and texture, then add to scene cells
+			var newmesh = floormeshes[type].duplicate()
 			var meshinstance = newmesh.get_node("MeshInstance")
 			var floor_material = UW.data["uw1"]["rotating_palette_spatial"].duplicate()
 			newmesh.translation.x = x*tilesize
